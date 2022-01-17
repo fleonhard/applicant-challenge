73be1620164febaf156e581dc9b799440fefdab6
Stefan Hering <stefan.hering@rewe-digital.com>
2022-01-17 10:09:46 +0100

    Initial commit

---
+++ src/main/java/com/rewe/digital/applicantchallenge/ApplicantChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.rewe.digital.applicantchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicantChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicantChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rewe/digital/applicantchallenge/ApplicantChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rewe.digital.applicantchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicantChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
